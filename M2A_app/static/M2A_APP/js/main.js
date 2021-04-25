e76c25848b1412972010db75253bf2f8f3951661
RafaelGuido <rafatocco7@gmail.com>
2021-04-25 17:59:08 -0300

    mascara de cpf

--- M2A_app/static/M2A_APP/js/main.js
+++ M2A_app/static/M2A_APP/js/main.js
@@ -9,3 +9,9 @@ var tel = document.getElementById("tel");
 
 var iml = new Inputmask("(99) 9999-9999");
 iml.mask(tel);
+
+//máscara cpf
+var cpf = document.getElementById("cpf");
+
+var imf = new Inputmask("999.999.999-99");
+imf.mask(cpf);
