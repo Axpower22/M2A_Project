69692a0734a5afef52033dd0e17b2fc0eba31a51
RafaelGuido <rafatocco7@gmail.com>
2021-06-14 21:07:16 -0300

    arrumando máscaras tela de cadastro de empresa

--- M2A_app/static/M2A_APP/js/main.js
+++ M2A_app/static/M2A_APP/js/main.js
@@ -2,28 +2,46 @@
 var cel = document.getElementById("cel");
 
 var imc = new Inputmask("(99) 99999-9999");
-imc.mask(cel);
+if(cel){
+    imc.mask(cel);
+}
 
 //máscara telefone
 var tel = document.getElementById("tel");
 
 var iml = new Inputmask("(99) 9999-9999");
-iml.mask(tel);
+if(tel){
+    iml.mask(tel);
+}
+
+//máscara fax
+var fax = document.getElementById("fax");
+
+var imx = new Inputmask("(99) 9999-9999");
+if(fax){
+    imx.mask(fax);
+}
 
 //máscara cpf
 var cpf = document.getElementById("cpf");
 
 var imf = new Inputmask("999.999.999-99");
-imf.mask(cpf);
+if(cpf){
+    imf.mask(cpf);
+}
 
 //máscara cnpj
 var cnpj = document.getElementById("cnpj");
 
 var imj = new Inputmask("99.999.999/9999-99");
-imj.mask(cnpj);
+if(cnpj){
+    imj.mask(cnpj);
+}
 
 //máscara cep
 var cep = document.getElementById("cep");
 
 var ime = new Inputmask("99999-999");
-imj.mask(cep);
+if(cep){
+    ime.mask(cep);
+}
