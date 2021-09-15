ab0042e1b82738eface29e11462241ebb19c53ba
RafaelGuido <rafatocco7@gmail.com>
2021-09-15 20:43:14 -0300

    arrumando erros do models.py

--- M2A_app/models.py
+++ M2A_app/models.py
@@ -3,24 +3,24 @@ from localflavor.br.models import BRCNPJField, BRPostalCodeField
 
 
 class UF(Model):
-    sg_uf = CharField()
-    nome_uf = CharField()
+    sg_uf = CharField(max_length=2)
+    nome_uf = CharField(max_length=500)
 
 
 class Setor(Model):
-    ds_setor = CharField()
+    ds_setor = CharField(max_length=500)
 
 
 class Segmento(Model):
-    ds_segmento = CharField()
+    ds_segmento = CharField(max_length=500)
 
 
 class ValorArrecadacao(Model):
-    ds_valor_arrecadacao = CharField()
+    ds_valor_arrecadacao = CharField(max_length=500)
 
 
 class TipoIndustria(Model):
-    ds_tipo_industria = CharField()
+    ds_tipo_industria = CharField(max_length=500)
 
 
 class Telefone(Model):
@@ -33,35 +33,35 @@ class Telefone(Model):
         ('FAX', 'Fax'),
     ]
 
-    tipo_telefone = CharField(choices=TIPO_TELEFONE_CHOICES)
+    tipo_telefone = CharField(choices=TIPO_TELEFONE_CHOICES, max_length=500)
 
 
 class Perfil(Model):
-    nm_perfil = CharField()
+    nm_perfil = CharField(max_length=500)
     bool_admin = BooleanField()
     bool_excluido = BooleanField()
 
 
 class Situacao(Model):
-    nome_situacao = CharField()
+    nome_situacao = CharField(max_length=500)
 
 
 class Usuario(Model):
-    nome = CharField()
+    nome = CharField(max_length=500)
     email = EmailField()
-    uf = ForeignKey(UF, on_delete=SET_NULL)
-    telefone = ForeignKey(Telefone, on_delete=SET_NULL)
-    situacao = ForeignKey(Situacao, on_delete=SET_NULL)
+    uf = ForeignKey(UF, on_delete=SET_NULL, null=True)
+    telefone = ForeignKey(Telefone, on_delete=SET_NULL, null=True)
+    situacao = ForeignKey(Situacao, on_delete=SET_NULL, null=True)
 
 
 class Grupo(Model):
-    nome_grupo = CharField()
+    nome_grupo = CharField(max_length=500)
 
 
 class Empresa(Model):
     cnpj = BRCNPJField()
-    razao_social = CharField()
-    fantasia = CharField()
+    razao_social = CharField(max_length=500)
+    fantasia = CharField(max_length=500)
     bool_master = BooleanField()
     inscricao_estadual = IntegerField()
     num_empregados = IntegerField()
@@ -70,43 +70,43 @@ class Empresa(Model):
 
     dt_ano_inicio = DateField()
 
-    projeto = CharField()
-    nome_gestor = CharField()
+    projeto = CharField(max_length=500)
+    nome_gestor = CharField(max_length=500)
 
     grupo = ForeignKey(Grupo, on_delete=SET_NULL, null=True)
 
     # INFORMAÇÕES COMPLEMENTARES
-    ds_negocio = CharField()
+    ds_negocio = CharField(max_length=500)
     bool_missao = BooleanField()
-    missao = CharField(blank=True)
+    missao = CharField(blank=True, max_length=500)
     bool_visao = BooleanField()
-    visao = CharField(blank=True)
+    visao = CharField(blank=True, max_length=500)
     bool_valores = BooleanField()
-    valores = CharField(blank=True)
+    valores = CharField(blank=True, max_length=500)
 
     # FOREIGN KEYS
-    fk_tipo_master = ForeignKey("self", on_delete=SET_NULL)
-    fk_segmento = ForeignKey(Segmento, on_delete=SET_NULL)
-    fk_setor = ForeignKey(Setor, on_delete=SET_NULL)
-    fk_valor_arrecadacao = ForeignKey(ValorArrecadacao, on_delete=SET_NULL)
-    fk_tipo_industria = ForeignKey(TipoIndustria, on_delete=SET_NULL)
+    fk_tipo_master = ForeignKey("self", on_delete=SET_NULL, null=True)
+    fk_segmento = ForeignKey(Segmento, on_delete=SET_NULL, null=True)
+    fk_setor = ForeignKey(Setor, on_delete=SET_NULL, null=True)
+    fk_valor_arrecadacao = ForeignKey(ValorArrecadacao, on_delete=SET_NULL, null=True)
+    fk_tipo_industria = ForeignKey(TipoIndustria, on_delete=SET_NULL, null=True)
 
     # ENDEREÇO
     cep = BRPostalCodeField()
-    logradouro = CharField()
-    endereco = CharField()
+    logradouro = CharField(max_length=500)
+    endereco = CharField(max_length=500)
 
 
 class TipoDiagnostico(Model):
-    nome_tipo_diagnostico = CharField()
+    nome_tipo_diagnostico = CharField(max_length=500)
 
 
 class FaseProjeto(Model):
-    nome_fase = CharField()
+    nome_fase = CharField(max_length=500)
 
 
 class Diagnostico(Model):
     dt_ano = DateField()
-    tipo = ForeignKey(TipoDiagnostico, on_delete=SET_NULL)
-    fk_empresa = ForeignKey(Empresa, on_delete=SET_NULL)
-    fase = ForeignKey(FaseProjeto, on_delete=SET_NULL)
+    tipo = ForeignKey(TipoDiagnostico, on_delete=SET_NULL, null=True)
+    fk_empresa = ForeignKey(Empresa, on_delete=SET_NULL, null=True)
+    fase = ForeignKey(FaseProjeto, on_delete=SET_NULL, null=True)
